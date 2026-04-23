f71a79fc01f6354c0a366968b4591de2c98bba27
AndorPakot <85230130+tweeeki@users.noreply.github.com>
2026-04-23 21:23:27 +0200

    added reach deploy.yaml deployment in wandb
    added reach deploy.yaml deployment in wandb

--- src/tasks/reaching/config/g1/__init__.py
+++ src/tasks/reaching/config/g1/__init__.py
@@ -1,4 +1,5 @@
 from mjlab.tasks.registry import register_mjlab_task
+from src.tasks.reaching.rl import ReachingOnPolicyRunner
 
 from .env_cfgs import unitree_g1_reach_env_cfg
 from .rl_cfg import unitree_g1_reach_ppo_runner_cfg
@@ -8,4 +9,5 @@ register_mjlab_task(
   env_cfg=unitree_g1_reach_env_cfg(),
   play_env_cfg=unitree_g1_reach_env_cfg(play=True),
   rl_cfg=unitree_g1_reach_ppo_runner_cfg(),
+  runner_cls=ReachingOnPolicyRunner,
 )
